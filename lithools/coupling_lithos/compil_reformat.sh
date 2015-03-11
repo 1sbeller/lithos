36aa26c972ceab787c2a0380ef9eeb52bb85249e
Stephen.BELLER <beller@kos.unice.fr>
2015-03-11 20:10:14 +0100

    modify

--- lithools/coupling_lithos/compil_reformat.sh
+++ lithools/coupling_lithos/compil_reformat.sh
@@ -4,7 +4,6 @@ F90='mpif90 -O3 -ipo -xHost -assume byterecl'
 
 $F90 -c ../../common/precision_mod.f90
 $F90 -c ../../common/constants_mod.f90
-$F90 -c gll_library.f90
 $F90 -c interpolation_parameters_mod.f90
 $F90 -c mpi_int_mod.f90
 $F90 -c interp_process_mod.f90
@@ -12,4 +11,17 @@ $F90 -c inputs_outputs_interp_mod.f90
 $F90 -c interpolate_3D_wavefield.f90
 $F90 *.o -o interpolate_3D_wavefield.x
  
+./clean.sh
+
+$F90 -c ../../common/precision_mod.f90
+$F90 -c ../../common/constants_mod.f90
+$F90 -c gll_library.f90
+$F90 -c mpi_mod.f90
+$F90 -c global_parameters_mod.f90
+$F90 -c rotation_matrix_mod.f90
+$F90 -c inputs_outputs_mod.f90
+$F90 -c interp_mesh_mod.f90
+$F90 -c post_process_wavefields_mod.f90
+$F90 -c reconstruct_3D_wavefield.f90
+$F90 *.o -o reconstruct_3D_wavefield.x
 
